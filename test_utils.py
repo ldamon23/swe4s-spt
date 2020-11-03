fa8731cd5cd0d942d3eae9db2a2f7cda62e70ab4
leda4342 <leda4342@colorado.edu>
2020-11-03 09:57:06 -0700

    began developing code to convert ND2 to TIF

--- test_utils.py
+++ test_utils.py
@@ -3,12 +3,36 @@
 """
 
 import unittest
-import open_file
+import utils
+import sys
+import matplotlib.pyplot as plt
 
-
-class TestMyUtils_open_file(unittest.TestCase):
-    """ Testing the functionality of open_file
+class TestUtils_utils(unittest.TestCase):
+    """ Testing the functionality of functions in utils
 
     """
 
-    def test_open_file(self):
+    def test_convert_ND2_missingfile(self):
+        # check for missing file, throw an error
+
+        file_in = 'crapFile.nd2'
+        file_out = 'crapFile.tif'
+        frame_range = [0]
+
+        with self.assertRaises(SystemExit) as ex:
+           utils.convert_ND2(file_in, file_out, frame_range)
+        self.assertEqual(ex.exception.code, 1)
+
+    def test_convert_ND2_disp_first_frame(self):
+        # check that we can load at minimum one frame
+        
+        file_in = 'sample_SPT.nd2'
+        file_out = 'sample_SPT.tif'
+        frame_range = [0]
+        img = utils.convert_ND2(file_in, file_out, frame_range)
+        
+        self.assertIsNotNone(img)
+
+        
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,18 +1,72 @@
-""" Open file using OpenCV2
+""" Utilty functions for SPT project
 """
 
+import sys
 import cv2
 import numpy as np
 from nd2reader import ND2Reader
 import matplotlib.pyplot as plt
+import warnings
 
-with ND2Reader('test_SPT.nd2') as images:
-    # plt.imshow(images[0])
-    print(images.sizes)
+def convert_ND2(file_in, file_out, frame_range):
+    """ Because ND2s are a pain to work with, convert to TIF
     
-    images.iter_axes = 'z'
+    Parameters:
+    file_in     : ND2 image to be processed
+    file_out    : Name of the output TIF
     
-    for frame in range(len(images)):
-        print('Frame is:' + str(frame))
-        plt.imshow(images[frame])
-        plt.set_cmap('gray')
+    outputs:
+    img_out    : converted TIF file
+    """
+    
+    # check that the file exists
+    try:
+        img = ND2Reader(file_in)
+    except:
+        print("Could not find file " + file_in)
+        sys.exit(1)
+
+    img = ND2Reader(file_in)
+    
+    for i in range(len(frame_range)):
+        plt.imshow(img[i])
+#     plt.imshow(img[frame_range])
+    # iterate over each frame
+    with ND2Reader(file_in) as images:
+        print(images.sizes)
+        # img = cv2.imshow('image',images[0])
+        
+#        img = plt.imshow(images[0])
+#        img = plt.set_cmap('gray')
+#        plt.savefig(file_out, bbox_inches='tight')
+
+        images.iter_axes = 'z'
+
+#         for frame in range(len(images)):
+#             print('Frame is:' + str(frame))
+#             plt.imshow(images[frame])
+#             plt.set_cmap('gray')
+
+    return img
+    
+    
+    
+#def open_file_ND2:
+#    """ Use ND2Reader if a file is ND2"""
+#
+#    with ND2Reader('test_SPT.nd2') as images:
+#        # plt.imshow(images[0])
+#        print(images.sizes)
+#        
+#        images.iter_axes = 'z'
+#        
+#        for frame in range(len(images)):
+#            print('Frame is:' + str(frame))
+#            plt.imshow(images[frame])
+#            plt.set_cmap('gray')
+#
+#def open_file_TIF
+#    """ Use CV2 if a file is TIF """
+
+
+    
