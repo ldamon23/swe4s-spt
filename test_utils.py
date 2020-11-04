07ddc0d6fe56557b26d70878cfe30e4eb3f596f8
leda4342 <leda4342@colorado.edu>
2020-11-04 14:35:20 -0700

    adding function and test to pull out diffusion coeff for 1 track

--- test_utils.py
+++ test_utils.py
@@ -9,68 +9,117 @@ import matplotlib.pyplot as plt
 import tifffile
 from nd2reader import ND2Reader
 
-class TestUtils_utils(unittest.TestCase):
-    """ Testing the functionality of functions in utils
+# class TestUtils_convert_ND2(unittest.TestCase):
+#     """ Testing the functionality of convert_ND2
+
+#     """
+
+#     def test_convert_ND2_missingfile(self):
+#         # check for missing file, throw an error
+
+#         file_in = 'crapFile.nd2'
+#         file_out = 'crapFile.tif'
+#         frame_range = [0] # load first frame, if possible
+
+#         with self.assertRaises(SystemExit) as ex:
+#            utils.convert_ND2(file_in, file_out, frame_range)
+#         self.assertEqual(ex.exception.code, 1)
+
+#     def test_convert_ND2_disp_first_frame(self):
+#         # check that we can load at minimum one frame
+        
+#         file_in = 'sample_SPT.nd2'
+#         file_out = 'sample_SPT.tif'
+#         frame_range = [0]
+#         img = utils.convert_ND2(file_in, file_out, frame_range)
+        
+#         self.assertIsNotNone(img)
+
+#     def test_convert_ND2_load_multiple_frames(self):
+#         # check that we can load 2+ frames
+        
+#         file_in = 'sample_SPT.nd2'
+#         file_out = 'sample_SPT.tif'
+#         frame_range = range(0,10)  # load first 10 frames - note: indexing starts at 1
+        
+#         utils.convert_ND2(file_in, file_out, frame_range)
+        
+#         # verify that a file was created and that it has the specified number of frames
+#         file = open('sample_SPT.tif')
+#         self.assertIsNotNone(file)
+#         file.close
+        
+#         tif = tifffile.TiffFile('sample_SPT.tif')
+#         self.assertEqual(len(tif.pages),10)
+        
+#     def test_convert_ND2_write_all_frames(self):
+#         # check that we can load all frames
+        
+#         file_in = 'sample_SPT.nd2'
+#         file_out = 'sample_SPT.tif'
+#         frame_range = 'all'  # load all frames
+        
+#         utils.convert_ND2(file_in, file_out, frame_range)
+
+#         # verify that a file was created and that it has the specified number of frames
+#         file = open(file_out)
+#         self.assertIsNotNone(file)
+#         file.close
+        
+#         img = ND2Reader(file_in)
+#         total_frames = (img.sizes['z'])
+        
+#         tif = tifffile.TiffFile('sample_SPT.tif')
+#         self.assertEqual(len(tif.pages),total_frames)
+        
+
+class TestUtils_calc_diffusion(unittest.TestCase):
+    """ Testing the functionality of the function calc_diffusion
 
     """
 
-    def test_convert_ND2_missingfile(self):
+    def test_calc_diffusion_missingfile(self):
         # check for missing file, throw an error
 
-        file_in = 'crapFile.nd2'
-        file_out = 'crapFile.tif'
-        frame_range = [0] # load first frame, if possible
+        file_in = 'crapFile.csv'  # I'm assuming I'll have a CSV, similar to mosaic
+        file_out = file_in[:-4] + '_diffusion_coeffs.csv'
+        traj_ID = 1
+        query_column = 1
+        results_columns = [3, 4]
 
         with self.assertRaises(SystemExit) as ex:
-           utils.convert_ND2(file_in, file_out, frame_range)
+           utils.calc_diffusion(file_in, file_out, query_column, traj_ID, results_columns)
         self.assertEqual(ex.exception.code, 1)
+        
+    def test_calc_diffusion_look_at_single_traj(self):
+        # identify a single trajectory, maybe pull out X-Y coords?
 
-    def test_convert_ND2_disp_first_frame(self):
-        # check that we can load at minimum one frame
-        
-        file_in = 'sample_SPT.nd2'
-        file_out = 'sample_SPT.tif'
-        frame_range = [0]
-        img = utils.convert_ND2(file_in, file_out, frame_range)
-        
-        self.assertIsNotNone(img)
+        file_in = 'sample_traj.csv'  # I'm assuming I'll have a CSV, similar to mosaic
+        file_out = file_in[:-4] + '_diffusion_coeffs.csv'
+        traj_ID = 1
+        query_column = 1
+        result_columns = [3, 4]
 
-    def test_convert_ND2_load_multiple_frames(self):
-        # check that we can load 2+ frames
+        traj_xy, diffusion = utils.calc_diffusion(file_in, file_out, query_column, traj_ID, result_columns, deltaT=0.1)
         
-        file_in = 'sample_SPT.nd2'
-        file_out = 'sample_SPT.tif'
-        frame_range = range(0,10)  # load first 10 frames - note: indexing starts at 1
-        
-        utils.convert_ND2(file_in, file_out, frame_range)
-        
-        # verify that a file was created and that it has the specified number of frames
-        file = open('sample_SPT.tif')
-        self.assertIsNotNone(file)
-        file.close
-        
-        tif = tifffile.TiffFile('sample_SPT.tif')
-        self.assertEqual(len(tif.pages),10)
-        
-    def test_convert_ND2_write_all_frames(self):
-        # check that we can load all frames
-        
-        file_in = 'sample_SPT.nd2'
-        file_out = 'sample_SPT.tif'
-        frame_range = 'all'  # load all frames
-        
-        utils.convert_ND2(file_in, file_out, frame_range)
+        expected = [['170.202', '22.481'], 
+                    ['169.726', '22.459'], 
+                    ['169.192', '22.727'],
+                    ['165.640', '22.686']]
 
-        # verify that a file was created and that it has the specified number of frames
-        file = open(file_out)
-        self.assertIsNotNone(file)
-        file.close
+        self.assertEqual(traj_xy, expected)
+    
+    def test_calc_diffusion_get_diffusion(self):
+        # compute the diffusion coefficient for a trajectory
         
-        img = ND2Reader(file_in)
-        total_frames = (img.sizes['z'])
-        
-        tif = tifffile.TiffFile('sample_SPT.tif')
-        self.assertEqual(len(tif.pages),total_frames)
+        file_in = 'sample_traj.csv'  # I'm assuming I'll have a CSV, similar to mosaic
+        file_out = file_in[:-4] + '_diffusion_coeffs.csv'
+        traj_ID = 1
+        query_column = 1
+        result_columns = [3, 4]
 
+        traj_xy, diffusion = utils.calc_diffusion(file_in, file_out, query_column, traj_ID, result_columns, deltaT=0.1)
+        
+        self.assertIsNotNone(diffusion)  # not sure what a better test would be for this...
 if __name__ == '__main__':
     unittest.main()
--- utils.py
+++ utils.py
@@ -8,13 +8,6 @@ from nd2reader import ND2Reader
 import matplotlib.pyplot as plt
 import tifffile
 
-# from PIL import Image
-# import sys
-# import numpy as np
-# from nd2reader import ND2Reader
-# import matplotlib.pyplot as plt
-# import tifffile
-
 def convert_ND2(file_in, file_out, frame_range='all'):
     """ Because ND2s are a pain to work with, convert to TIF
     
@@ -54,4 +47,75 @@ def convert_ND2(file_in, file_out, frame_range='all'):
     return output_img
 
 
+def calc_diffusion(file_in, file_out, query_column, traj_ID, result_columns, deltaT=0.1):  # should change traj_ID to default to 'all' to ensure looping
+    
+    # check that the file exists
+    try:
+        traj_file = open(file_in, 'r') # open file with traj info
+    except:
+        print("Could not find file " + file_in)
+        sys.exit(1)
+    
+    dataOut = []  # intialize empty list of lists for storage
+    header = None
+
+    
+    for line in traj_file:
+        if header is None:
+            header = line
+            continue
+        currLine = line.rstrip().split(',')
+        try:
+            if currLine[query_column] == str(traj_ID):
+                data = []
+                for j in result_columns:
+                    data.append(currLine[j])
+                dataOut.append(data)
+        except IndexError:
+            print("Asking for query column "
+                  + str(query_column)
+                  + " and results column "
+                  + str(result_columns)
+                  + ", but there are only "
+                  + str(len(currLine))
+                  + " fields")
+        except ValueError:
+            print('test')
+    
+    # calculate mean squared displacement(MSD) for a single trajectory
+    # assuming 2D Brownian diffusion, we can simplify the equation to be:
+    # MSD = 4*D*deltaT
+    # where D is the diffusion coeff and deltaT is the time delay between frames
+
+    # first, transform dataOut to numpy array to make it easier to index (at least for me)
+    # I have a feeling this is a messy way to do it; so I'm open to suggestions on cleaning up!
+
+    dataOut2 = np.array(dataOut)
+    xdata = []
+    ydata = []
+    for i in range(len(dataOut2)):
+        xdata.append(float(dataOut2[i,0]))
+    for i in range(len(dataOut2)):
+        ydata.append(float(dataOut2[i,1]))
+
+    # convert back to np array (can't do below math on a list)
+    xdata = np.array(xdata)/1000  # converting from nm to um (convention)
+    ydata = np.array(ydata)/1000  # converting from nm to um (convention)
+    # now, calculate the displacements
+    r = np.sqrt(xdata**2 + ydata**2)
+    diff = np.diff(r) #this calculates r(t + dt) - r(t)
+    diff_sq = diff**2
+    MSD = np.mean(diff_sq)
+
+    # diffusion can now be computed
+    # note: this is just the "average" diffusion throughout the length of the track
+    # as we're assuming it's Brownian motion (for simplicity). A better way would be to fit
+    # to a specific model of diffusion (anomolous/constrained, for instance).. 
+    # but for now, simplicity rules
+    deltaT = 0.1  # seconds
+    diffusion = (MSD)/(4 * deltaT)
+
+    traj_file.close()
+
+    return dataOut, diffusion
     
