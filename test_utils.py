2cd1a745e004eca22118547d383b4dbeb0667ce8
Jeremy Brown <jermybrown09@gmail.com>
2020-11-07 01:35:30 +0000

    Brought all code under PEP-8 guidelines. Fixed process_image so that now it will use gaussian blur on all frames before returning them if gBlur is True.

--- test_utils.py
+++ test_utils.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import tifffile
 from nd2reader import ND2Reader
 
+
 class TestUtils_utils(unittest.TestCase):
     """ Testing the functionality of functions in utils
 
@@ -19,58 +20,70 @@ class TestUtils_utils(unittest.TestCase):
 
         file_in = 'crapFile.nd2'
         file_out = 'crapFile.tif'
-        frame_range = [0] # load first frame, if possible
+        frame_range = [0]  # load first frame, if possible
 
         with self.assertRaises(SystemExit) as ex:
-           utils.convert_ND2(file_in, file_out, frame_range)
+            utils.convert_ND2(file_in, file_out, frame_range)
         self.assertEqual(ex.exception.code, 1)
 
     def test_convert_ND2_disp_first_frame(self):
         # check that we can load at minimum one frame
-        
+
         file_in = 'sample_SPT.nd2'
         file_out = 'sample_SPT.tif'
         frame_range = [0]
         img = utils.convert_ND2(file_in, file_out, frame_range)
-        
+
         self.assertIsNotNone(img)
 
     def test_convert_ND2_load_multiple_frames(self):
         # check that we can load 2+ frames
-        
+
         file_in = 'sample_SPT.nd2'
         file_out = 'sample_SPT.tif'
-        frame_range = range(0,10)  # load first 10 frames - note: indexing starts at 1
-        
+        # load first 10 frames - note: indexing starts at 1
+        frame_range = range(0, 10)
+
         utils.convert_ND2(file_in, file_out, frame_range)
-        
-        # verify that a file was created and that it has the specified number of frames
+
+        # verify that a file was created and
+        # that it has the specified number of frames
         file = open('sample_SPT.tif')
         self.assertIsNotNone(file)
         file.close
-        
+
         tif = tifffile.TiffFile('sample_SPT.tif')
-        self.assertEqual(len(tif.pages),10)
-        
+        self.assertEqual(len(tif.pages), 10)
+
     def test_convert_ND2_write_all_frames(self):
         # check that we can load all frames
-        
+
         file_in = 'sample_SPT.nd2'
         file_out = 'sample_SPT.tif'
         frame_range = 'all'  # load all frames
-        
+
         utils.convert_ND2(file_in, file_out, frame_range)
 
-        # verify that a file was created and that it has the specified number of frames
+        # verify that a file was created and
+        # that it has the specified number of frames
         file = open(file_out)
         self.assertIsNotNone(file)
         file.close
-        
+
         img = ND2Reader(file_in)
         total_frames = (img.sizes['z'])
-        
+
         tif = tifffile.TiffFile('sample_SPT.tif')
-        self.assertEqual(len(tif.pages),total_frames)
+        self.assertEqual(len(tif.pages), total_frames)
+
+    def test_process_image(self):
+        # check that process image isn't throwing any errors
+
+        file_in = 'sample_SPT.tif'
+        result = None
+        result = utils.process_image(file_in)
+        self.assertIsNotNone(result)
+
 
 if __name__ == '__main__':
     unittest.main()
--- utils.py
+++ utils.py
@@ -2,56 +2,93 @@
 """
 
 import sys
-import sys
+import subprocess
+try:
+    import cv2 as cv
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m",
+                           "pip", "install", 'opencv-python'])
+    import cv2 as cv
 import numpy as np
 from nd2reader import ND2Reader
 import matplotlib.pyplot as plt
 import tifffile
-
+from skimage import io
+import pims
 # from PIL import Image
-# import sys
-# import numpy as np
-# from nd2reader import ND2Reader
-# import matplotlib.pyplot as plt
-# import tifffile
+
 
 def convert_ND2(file_in, file_out, frame_range='all'):
     """ Because ND2s are a pain to work with, convert to TIF
-    
+
     Parameters:
     file_in     : ND2 image to be processed
     file_out    : Name of the output TIF
     frame_range : Frame range to iterate over; defaults to 'all'
-    
+
     outputs:
     img_out    : converted TIF file
     """
-    
+
     # check that the file exists
     try:
         img = ND2Reader(file_in)
-    except:
+    except FileNotFoundError:
         print("Could not find file " + file_in)
         sys.exit(1)
 
-    img = ND2Reader(file_in)
-    img.iter_axes = 'z' # t oddly does not work; z-steps instead
-    
+    img.iter_axes = 'z'  # t oddly does not work; z-steps instead
+
     # handle cases where we want to process all frames
     if frame_range == 'all':
         frame_range = range(img.sizes['z'])
-    
+
     for index, frame in enumerate(frame_range):
         if index == 0:
-            print('Curr frame is: ' + str(frame))
+            print('Processing frames[0.', end='')
             output_img = np.array(img[frame], dtype='uint16')
             tifffile.imwrite(file_out, output_img)
         else:
-            print('Curr frame is: ' + str(frame))
-            output_img = np.array(img[frame], dtype='uint16')  # take frame and store as np array
+            print('.', end='')
+            # take frame and store as np array
+            output_img = np.array(img[frame], dtype='uint16')
             tifffile.imwrite(file_out, output_img, append=True)
+    print('.' + str(frame) +
+          ']     Done. Curr frame is: ' +
+          str(frame) + '    ')
+    img.close()
 
     return output_img
 
 
-    
+def process_image(file_name, blurIter=1, gBlur=True, tif_stack=True):
+    '''Use image analysis algorithms to extract features from an image
+
+    Parameters:
+    file_name    :name of the file to be processed
+    tif_stack     :decides whether to process image as a tif stack
+    blurIter    :# of iterations the gaussian blur should be applied
+    gBlur    :boolean decider for gaussian blur application
+    tif_stack    :boolean decider to handle file as a tif stack
+
+    Outputs:
+    results    :list of processed frames from the video
+    '''
+    results = []
+    if tif_stack:
+        try:
+            with pims.TiffStack(file_name) as images:
+                for i in range(len(images)):
+                    img = images[i]
+                    if gBlur:
+                        img = cv.GaussianBlur(img, (5, 5), blurIter)
+                    cv.imwrite('out/' + str(i) + '.png', img)
+                    results.append(img)
+        except FileNotFoundError:
+            print("Could not find file " + file_name)
+            sys.exit(1)
+    else:
+        print('Only tif stacks are supported currently')
+        sys.exit(1)
+
+    return results
